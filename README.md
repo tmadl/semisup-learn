3822958ee48f7548b6ec99e0739563e79556fad6
Tamas Madl <thomas.madl@gmail.com>
2015-07-28 16:55:43 +0100

    clarification

--- README.md
+++ README.md
@@ -52,20 +52,20 @@ basemodel = SGDClassifier(loss='log', penalty='l1') # scikit logistic regression
 basemodel.fit(X[random_labeled_points, :], ys[random_labeled_points])
 print "supervised log.reg. score", basemodel.score(X, ytrue)
 
-# semi-supervised score (base model has to be able to take weighted samples)
-ssmodel = CPLELearningModel(basemodel)
-ssmodel.fit(X, ys)
-print "semi-supervised log.reg. score", ssmodel.score(X, ytrue)
-
 # fast (but naive, unsafe) self learning framework
 ssmodel = SelfLearningModel(basemodel)
 ssmodel.fit(X, ys)
 print "self-learning log.reg. score", ssmodel.score(X, ytrue)
 
+# semi-supervised score (base model has to be able to take weighted samples)
+ssmodel = CPLELearningModel(basemodel)
+ssmodel.fit(X, ys)
+print "CPLE semi-supervised log.reg. score", ssmodel.score(X, ytrue)
+
 # semi-supervised score, RBF SVM model
 ssmodel = CPLELearningModel(sklearn.svm.SVC(kernel="rbf", probability=True), predict_from_probabilities=True) # RBF SVM
 ssmodel.fit(X, ys)
-print "semi-supervised RBF SVM score", ssmodel.score(X, ytrue)
+print "CPLE semi-supervised RBF SVM score", ssmodel.score(X, ytrue)
 
 # supervised log.reg. score 0.418518518519
 # self-learning log.reg. score 0.444444444444
--- examples/example.py
+++ examples/example.py
@@ -26,22 +26,22 @@ basemodel = SGDClassifier(loss='log', penalty='l1') # scikit logistic regression
 basemodel.fit(X[random_labeled_points, :], ys[random_labeled_points])
 print "supervised log.reg. score", basemodel.score(X, ytrue)
 
-# semi-supervised score (base model has to be able to take weighted samples)
-ssmodel = CPLELearningModel(basemodel)
-ssmodel.fit(X, ys)
-print "semi-supervised log.reg. score", ssmodel.score(X, ytrue)
-
 # fast (but naive, unsafe) self learning framework
 ssmodel = SelfLearningModel(basemodel)
 ssmodel.fit(X, ys)
 print "self-learning log.reg. score", ssmodel.score(X, ytrue)
 
+# semi-supervised score (base model has to be able to take weighted samples)
+ssmodel = CPLELearningModel(basemodel)
+ssmodel.fit(X, ys)
+print "CPLE semi-supervised log.reg. score", ssmodel.score(X, ytrue)
+
 # semi-supervised score, WQDA model
 ssmodel = CPLELearningModel(WQDA(), predict_from_probabilities=True) # weighted Quadratic Discriminant Analysis
 ssmodel.fit(X, ys)
-print "semi-supervised WQDA score", ssmodel.score(X, ytrue)
+print "CPLE semi-supervised WQDA score", ssmodel.score(X, ytrue)
 
 # semi-supervised score, RBF SVM model
 ssmodel = CPLELearningModel(sklearn.svm.SVC(kernel="rbf", probability=True), predict_from_probabilities=True) # RBF SVM
 ssmodel.fit(X, ys)
-print "semi-supervised RBF SVM score", ssmodel.score(X, ytrue)
+print "CPLE semi-supervised RBF SVM score", ssmodel.score(X, ytrue)
